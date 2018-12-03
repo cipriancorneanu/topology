f8f7eb5761de170029c6783e66dc8307fd5e4564
CiprianCorneanu <CiprianCorneanu>
2018-12-03 23:37:52 +0100

    Adapting main to the new code factorization.

--- main.py
+++ main.py
@@ -8,12 +8,15 @@ import torch.backends.cudnn as cudnn
 import torchvision
 import os
 import argparse
-from models.densenet import *
+from models.lenet import LeNet
 from utils import progress_bar
 import numpy as np
 import h5py
 from utils import *
 import pickle as pkl
+from passers import Passer
+from savers import save_activations, save_checkpoint, save_losses
+from loaders import *
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--net')
@@ -28,30 +31,33 @@ parser.add_argument('--shuffle_labels', default=0, type=int)
 parser.add_argument('--fixed_init', default=0, type=int)
 parser.add_argument('--n_train_batches', default=0, type=int)
 parser.add_argument('--input_size', default=32, type=int)
-
-
 args = parser.parse_args()
+
+''' At what epochs to save train/test stats '''
+SAVE_EPOCHS = list(range(10)) + list(range(10, args.epochs, args.save_every))
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 1  # start from epoch 1 or last checkpoint epoch
 
+''' Prepare data '''
 if args.dataset=='cifar':
-    trainloader, testloader = prepare_cifar10()
+    trainloader, testloader = cifar10()
     criterion = nn.CrossEntropyLoss()
 elif args.dataset=='imagenet':
-    trainloader, testloader = prepare_tiny_imagenet(args.input_size)
+    trainloader, testloader = tinyimagenet(args.input_size)
     criterion = nn.CrossEntropyLoss()
 elif args.dataset=='mnist':
-    trainloader, testloader = prepare_mnist()
+    trainloader, testloader = mnist()
     criterion = F.nll_loss
 elif args.dataset=='mnist_adversarial':
-    testloader = prepare_mnist_adversarial()
+    testloader = mnist_adversarial()
     criterion = F.nll_loss
-    
+
+''' Meta-name to be used as prefix on all savings'''
 oname = args.net + '_' + args.dataset
 
-# Model
+# Build models
 print('==> Building model..')
 if args.net=='lenet' and args.dataset in ['mnist', 'cifar', 'mnist_adversarial']:
     net = LeNet(num_classes=10)
@@ -80,6 +86,7 @@ if device == 'cuda':
     net = torch.nn.DataParallel(net)
     cudnn.benchmark = True
 
+''' Initialize from checkpoint'''
 if args.fixed_init:
     # Load checkpoint.
     print('==> Initializing  from fixed checkpoint..')
@@ -88,7 +95,9 @@ if args.fixed_init:
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
+
     
+''' Resume from checkpoint'''
 if args.resume:
     # Load checkpoint.
     print('==> Resuming from checkpoint..')
@@ -97,32 +106,35 @@ if args.resume:
     net.load_state_dict(checkpoint['net'])
     best_acc = checkpoint['acc']
     start_epoch = checkpoint['epoch']
-        
+
+    
+''' Optimization '''
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 lr_scheduler = ReduceLROnPlateau(optimizer, factor=0.5, mode='max', verbose=True)
 
-activs, targets, loss_te, acc_te = test(net, testloader, device, criterion, n_test_batches=10)
-save_model(model = {'net':net.state_dict(), 'acc': acc_te, 'epoch': 0}, path='./checkpoint/', fname='ckpt_trial_'+str(args.trial)+'_epoch_0.t7')
-save_activations(activs, targets, path='./activations/'+oname, fname='activations_trial_'+str(args.trial)+'.hdf5', internal_path='epoch_0')
+''' Define passer'''
+passer = Passer(net, criterion, device)
 
-stats = []
+''' Make intial pass before any training '''
+activs, loss_te, acc_te = passer.test(testloader, forward_features=True)
+save_checkpoint(checkpoint = {'net':net.state_dict(), 'acc': acc_te, 'epoch': 0}, path='./checkpoint/', fname='ckpt_trial_'+str(args.trial)+'_epoch_0.t7')
+save_activations(activs, path='./activations/'+oname, fname='activations_trial_'+str(args.trial)+'.hdf5', internal_path='epoch_0')
+
+
+losses = []
 for epoch in range(start_epoch, start_epoch+args.epochs+1):
     print('Epoch {}'.format(epoch))
-    _, _ = train(net, trainloader, device, optimizer, criterion, do_optimization=True, shuffle_labels=args.shuffle_labels, n_batches=args.n_train_batches)
-    loss_tr, acc_tr = train(net, trainloader, device, optimizer, criterion, do_optimization=False,  shuffle_labels=args.shuffle_labels, n_batches=args.n_train_batches)
-    activs, targets, loss_te, acc_te = test(net, testloader, device, criterion, n_test_batches=10)
+    loss_tr, acc_tr = passer.train(trainloader, optimizer)
+    '''loss_tr, acc_tr = train(net, trainloader, device, optimizer, criterion, do_optimization=False,  shuffle_labels=args.shuffle_labels, n_batches=args.n_train_batches)'''
+    activs, loss_te, acc_te = passer.test(testloader, forward_features=True)
 
-    stats.append({'loss_tr':loss_tr, 'loss_te': loss_te, 'acc_tr': acc_tr, 'acc_te':acc_te})
+    losses.append({'loss_tr':loss_tr, 'loss_te': loss_te, 'acc_tr': acc_tr, 'acc_te':acc_te})
         
     lr_scheduler.step(acc_te)
 
-    if epoch<10:
-        save_model(model = {'net':net.state_dict(), 'acc': acc_te, 'epoch': epoch}, path='./checkpoint/', fname='ckpt_trial_'+str(args.trial)+'_epoch_'+str(epoch)+'.t7')
-        save_activations(activs, targets, path='./activations/'+oname, fname='activations_trial_'+str(args.trial)+'.hdf5', internal_path='epoch_'+str(epoch))  
-    elif epoch%args.save_every==0:
-        save_model(model = {'net':net.state_dict(), 'acc': acc_te, 'epoch': epoch}, path='./checkpoint/', fname='ckpt_trial_'+str(args.trial)+'_epoch_'+str(epoch)+'.t7')
-        save_activations(activs, targets, path='./activations/'+oname, fname='activations_trial_'+str(args.trial)+'.hdf5', internal_path='epoch_'+str(epoch))
-
+    if epoch in SAVE_EPOCHS:
+        save_checkpoint(checkpoint = {'net':net.state_dict(), 'acc': acc_te, 'epoch': epoch}, path='./checkpoint/', fname='ckpt_trial_'+str(args.trial)+'_epoch_'+str(epoch)+'.t7')
+        save_activations(activs, path='./activations/'+oname, fname='activations_trial_'+str(args.trial)+'.hdf5', internal_path='epoch_'+str(epoch))
         
 '''Save losses'''
-save_losses(stats, path='./losses/'+oname, fname='stats_trial_' +str(args.trial) +'.pkl')
+save_losses(losses, path='./losses/'+oname, fname='stats_trial_' +str(args.trial) +'.pkl')
