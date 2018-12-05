82097b0d358b4dacdabef2e42ea64497fc96d903
CiprianCorneanu <CiprianCorneanu>
2018-12-05 19:16:00 +0100

    Factorized data loading utilities.

--- loaders.py
+++ loaders.py
@@ -12,39 +12,101 @@ import torch
 import torchvision
 
 
-def mnist_adversarial():
-    ''' Return loader for adversarial mnist '''
-    transform = transforms.Compose([transforms.Grayscale(1),
-                                         transforms.ToTensor(),
-                                         transforms.Normalize((0.1307,), (0.3081,))])
+TRANSFORMS_TR_CIFAR10 = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
-    dataset = torchvision.datasets.ImageFolder(root='/data/data1/datasets/lenet_mnist_adversarial/', transform=transform)
+TRANSFORMS_TE_CIFAR10 = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+TRANSFORMS_TR_IMAGENET = transforms.Compose([
+    transforms.Resize(32),
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))       
+])
+
+TRANSFORMS_TE_IMAGENET = transforms.Compose([
+    transforms.Resize(32),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+])
+
+TRANSFORMS_MNIST_ADV = transforms.Compose([transforms.Grayscale(1),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.1307,), (0.3081,))])
+
+TRANSFORMS_MNIST = transforms.Compose([transforms.ToTensor(),
+                                      transforms.Normalize((0.1307,), (0.3081,))])
+
+
+def loader(data, subset=[]):
+    ''' Interface to the dataloader function '''
+    if data == 'mnist_train':
+        return dataloader('mnist', './data', train=True, transform=TRANSFORMS_MNIST, batch_size=128, shuffle=False, num_workers=2, subset=subset)
+    elif data == 'mnist_test':
+        return dataloader('mnist', './data', train=False, transform=TRANSFORMS_MNIST, batch_size=100, shuffle=False, num_workers=2, subset=subset)
+    elif data  == 'cifar10_train':
+        return dataloader('cifar10', './data', train=True, transform=TRANSFORMS_TR_CIFAR10, batch_size=128, shuffle=False, num_workers=2, subset=subset)
+    elif data == 'cifar10_test':
+        cifar10_te = dataloader('cifar10', './data', train=False, transform=TRANSFORMS_TE_CIFAR10, batch_size=100, shuffle=False, num_workers=2, subset=subset)
+    elif data == 'mnist_adversarial':
+        return dataloader('/data/data1/datasets/lenet_mnist_adversarial/', train=False,
+                          transform=TRANSFORMS_TE_CIFAR10, batch_size=100, shuffle=False, num_workers=2, subset=subset)
+    elif data == 'cifar10_adversarial':
+        return dataloader('/data/data1/datasets/lenet_cifar_adversarial/', train=False,
+                          transform=TRANSFORMS_TE_CIFAR10, batch_size=100, shuffle=False, num_workers=2, subset=subset)
+    elif data == 'imagenet_train':
+        return dataloader('tinyimagenet', '/data/data1/datasets/tiny-imagenet-200/train/',
+                                 train=True, transform=TRANSFORMS_TR_IMAGENET, batch_size=128, shuffle=True, num_workers=2, subset=subset)
+    elif loader == 'imagenet_test':
+        return dataloader('tinyimagenet', '/data/data1/datasets/tiny-imagenet-200/val/images/',
+                                 train=False, transform=TRANSFORMS_TE_IMAGENET, batch_size=100, shuffle=False, num_workers=2, subset=subset)
+
+
+def dataloader(data, path, train, transform, batch_size, shuffle, num_workers, subset=[]):
+    ''' Return loader for torchvision data '''
+    print('===> Preparing data...')
+    if data == 'mnist':
+        dataset = torchvision.datasets.MNIST(path, train=train, download=True, transform=transform)
+    elif data == 'cifar10':
+        dataset = torchvision.datasets.CIFAR10(path, train=train, download=True, transform=transform)
+    else:
+        dataset = torchvision.datasets.ImageFolder(path, transform=transform)
+
+    if subset : dataset = torch.utils.data.Subset(dataset, subset)
+    
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+
+    return loader
+
+
+
+
+
+'''
+def mnist_adversarial():
+    dataset = torchvision.datasets.ImageFolder(root='/data/data1/datasets/lenet_mnist_adversarial/', transform=TRANSFORM_MNIST_ADV)
     loader = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=False, num_workers=2)
 
     return loader
 
 
 def cifar_adversarial():
-    ''' Return loader for adversarial CIFAR10'''
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    dataset = torchvision.datasets.ImageFolder(root='/data/data1/datasets/cifar_adversarial/', transform=transform_test)
+    dataset = torchvision.datasets.ImageFolder(root='/data/data1/datasets/cifar_adversarial/', transform=TRANSFORMS_TE_CIFAR10)
     loader = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=False, num_workers=2)
 
     return loader
     
 
 def mnist():
-    ''' Return loaders for MNIST '''
     print('===> Preparing data...')
-    transform_train = transforms.Compose([transforms.ToTensor(),
-                                          transforms.Normalize((0.1307,), (0.3081,))])
-
-    transform_test = transforms.Compose([transforms.ToTensor(),
-                                         transforms.Normalize((0.1307,), (0.3081,))])
     
     trainset = torchvision.datasets.MNIST('../data', train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=False, num_workers=2)
@@ -54,21 +116,9 @@ def mnist():
 
     return trainloader, testloader
 
-    
-def cifar10():
-    ''' Return loaders for CIFAR10'''
-    print('===> Preparing data...')
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
 
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
+def cifar10():
+    print('===> Preparing data...')
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
@@ -76,28 +126,13 @@ def cifar10():
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
-    ''' Not used '''
     classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     return trainloader, testloader
 
 
 def tinyimagenet(input_size=32):
-    ''' Return loaders (train, test) for TinyImagenet'''
     print('==> Preparing data...')
-    transform_train = transforms.Compose([
-        transforms.Resize(input_size),
-        transforms.RandomCrop(input_size, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))       
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.Resize(input_size),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    ])
 
     trainset = torchvision.datasets.ImageFolder(root='/data/data1/datasets/tiny-imagenet-200/train/', transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
@@ -106,7 +141,7 @@ def tinyimagenet(input_size=32):
     testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
 
     return trainloader, testloader
-
+'''
 
 def validation_imagenet(data_dir, validation_labels_file):
     ''' What was this for? Is it used? '''
