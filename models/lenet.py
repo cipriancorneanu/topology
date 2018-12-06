c64bbd58d705feefd14aa8ea2c11c53d0d4e52d0
CiprianCorneanu <CiprianCorneanu>
2018-12-06 22:44:21 +0100

    LeNet modified to accecpt 32x32 inputs.

--- models/lenet.py
+++ models/lenet.py
@@ -9,13 +9,13 @@ class LeNet(nn.Module):
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(500, 50)
         self.fc2 = nn.Linear(50, num_classes)
 
     def forward(self, x):
         out = F.relu(F.max_pool2d(self.conv1(x), 2))
         out = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(out)), 2))
-        out = out.view(-1, 320)
+        out = out.view(-1, 500)
         out = F.relu(self.fc1(out))
         '''out = F.dropout(out, training=self.training)'''
         out = F.log_softmax(self.fc2(out), dim=1)
@@ -24,7 +24,7 @@ class LeNet(nn.Module):
     def forward_features(self, x):
         x1 = F.relu(F.max_pool2d(self.conv1(x), 2))
         x2 = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x1)), 2))
-        x2 = x2.view(-1, 320)
+        x2 = x2.view(-1, 500)
         x3 = F.relu(self.fc1(x2))
         x4 = F.log_softmax(self.fc2(x3), dim=1)
         return [x1, x2, x3, x4]
