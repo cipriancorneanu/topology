cd0f9d872b1d3b5c2bdc7b7823e38a9fdc1ce94a
CiprianCorneanu <CiprianCorneanu>
2019-01-10 23:49:37 +0100

    Adding plit adjacency computation for the case when signals too large.

--- graph.py
+++ graph.py
@@ -39,12 +39,22 @@ def corrpdf(signals):
     
     return pdf
 
+
 def adjacency_correlation(signals):
     ''' Faster version of adjacency matrix with correlation metric '''
     signals = np.reshape(signals, (signals.shape[0], -1))
     return np.nan_to_num(np.corrcoef(signals))
+
+
+def binarize(M, binarize_t):
+    ''' Binarize matrix. Real subunitary values. '''
+    corr[M>binarize_t] = 1
+    corr[M<=binarize_t] = 0
     
-def adjacency(signals, metric):
+    return M
+    
+
+def adjacency(signals, metric=None):
     '''
     Build matrix A  of dimensions nxn where a_{ij} = metric(a_i, a_j).
     signals: nxm matrix whre each row (signal[k], k=range(n)) is a signal. 
@@ -53,6 +63,10 @@ def adjacency(signals, metric):
     
     ''' Get input dimensions '''
     signals = np.reshape(signals, (signals.shape[0], -1))
+    ''' If no metric provided fast-compute correlation  '''
+    if not metric:
+        return np.nan_to_num(np.corrcoef(signals))
+        
     n, m = signals.shape
 
     A = np.zeros((n, n))
@@ -64,4 +78,73 @@ def adjacency(signals, metric):
             A[i,j] = metric(signals[i], np.transpose(signals[j]))
             
     return A
+
+
+def build_adjacency(signals, binarize_t=None):
+    '''
+    Build adjacency matrix
+    '''
     
+    ''' Compute adjacency (default metric is correlation)'''
+    A = adjacency(signals)
+    
+    ''' Binarize '''
+    if binarize_t:
+        A = binarize(corr, binarize_t)
+    
+    return A
+
+
+def build_adjacency_split(signals,  sz_chunk, binarize_t=None):
+    ''' 
+    Split signals into chunks and Build adjacency matrix for each.
+    signals: a list of signals. 
+    sz_chunk: size of chunks to split signal 
+    '''
+
+    out = []
+    for i_x, x in enumerate(signals):
+        sz = np.prod(np.shape(x)[1:])
+        
+        if sz > sz_chunk:
+            chunks = np.array_split(x, sz/sz_chunk, axis=1)
+        else:
+            chunks = [x]
+            
+        for i_chunk, chunk in enumerate(chunks):
+            nodes = np.transpose(chunk.reshape(chunk.shape[0], -1))
+            '''nodes = np.concatenate([np.transpose(a.reshape(a.shape[0], -1)) for a in activs], axis=0)'''
+
+            A = adjacency(nodes)
+
+            if binarize_t:
+                A = binarize(A, binarize_t)
+            
+            out.append({'layer':i_x, 'chunk': i_chunk, 'data': A})
+
+    return out
+
+def build_density_adjacency(activs, density_t):
+    nodes = np.concatenate([np.transpose(a.reshape(a.shape[0], -1)) for a in activs], axis=0)
+
+    ''' Compute correlation matrix '''
+    corr = np.nan_to_num(np.corrcoef(nodes))
+    total_edges = np.prod(corr.shape)
+    
+    
+    ''' Binarize matrix '''
+    t, t_decr = 1, 0.005
+    while True:
+        ''' Decrease threshold until density is met '''
+        edges = np.sum(corr > t)
+        density = edges/total_edges
+        ''' print('Threhold: {}; Density:{}'.format(t, density))'''
+        
+        if density > density_t:
+            corr[corr > t] = 1
+            corr[corr <= t] = 0
+            break
+
+        t = t-t_decr
+        
+    return corr, density, t
