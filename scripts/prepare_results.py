6227ab8dbd3532e76d8654002a2646796b007105
CiprianCorneanu <CiprianCorneanu>
2019-01-18 20:39:08 +0100

    Adding percentage of permutted labels and amount of data to the file name of the bettis file for idetification.

--- scripts/prepare_results.py
+++ scripts/prepare_results.py
@@ -9,6 +9,8 @@ parser.add_argument('--trial', default=0, type=int)
 parser.add_argument('--epochs', nargs='+', type=int)
 parser.add_argument('--thresholds', nargs='+')
 parser.add_argument('--n_bettis', type=int, default=3)
+parser.add_argument('--permute_labels', type=float)
+parser.add_argument('--subset', type=float)
 args = parser.parse_args()
 
 PATH = '/data/data1/datasets/cvpr2019/adjacency/' + args.net + '_' + args.dataset + '/'
@@ -32,8 +34,6 @@ for e in args.epochs:
 
     bettis['epc_{}'.format(e)] = epoch
 
-print(bettis)
-
 ''' Save bettis '''
-with open(PATH + 'bettis.pkl', "wb") as f:
+with open(PATH + 'bettis_p{}_s{}.pkl'.format(args.permute_labels, args.subset), "wb") as f:
     pickle.dump(bettis, f, pickle.HIGHEST_PROTOCOL)
