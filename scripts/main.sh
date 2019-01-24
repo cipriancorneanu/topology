bebef8308806e816e2fec5639e82ab097486b19b
CiprianCorneanu <CiprianCorneanu>
2019-01-24 22:54:21 +0100

    Several changes in arguments. Static variables moved to config.sh.

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,46 +4,46 @@
 function usage {
     echo "usage: $main [-net network_name] [-dt dataset_name] [-trl trial] [-netr n_epochs_train] [-ete epochs_test] [-plb permute_labels] [-dtsub datasubset]"
     echo "  -n  Specify deep network architecture (e.g. lenet, alexnet, resnet, inception, vgg, etc)"
-    echo "  -d   Specify dataset (e.g. mnist, cifar10, imagenet)"
-    echo "  -t    Specify trial number. Used to differentiate btw multiple trainings of same setup."
-    echo "  -e   Specify number of training epochs. "
-    echo "  -g    Specify list of epochs for which graph building is going to be performed. Sequence of positive integers delimited by blank space."
-    echo "  -p    Specify if labels are going to be permuted. Float between 0 and 1. If 0, no permutation. If 1 all labels are permuted. Otherwise proportion of labels."
-    echo "  -s  Specify if subset of data should be loaded. Float between 0 and 1. If 0, all data, else proportio of data randomly sampled. "
-   
+    echo "  -d  Specify dataset (e.g. mnist, cifar10, imagenet)"
+    echo "  -t  Specify trial number. Used to differentiate btw multiple trainings of same setup."
+    echo "  -l  Specify learnig rate for training. Recommendet: 0.001"
+    echo "  -e  Specify number of training epochs. "
+    echo "  -g  Specify list of epochs for which graph building is going to be performed. Sequence of positive integers delimited by blank space."
+    echo "  -p  Specify if labels are going to be permuted. Float between 0 and 1. If 0, no permutation. If 1 all labels are permuted. Otherwise proportion of labels."
+    echo "  -s  Specify if subset of data should be loaded. Float between 0 and 1. If 0, all data, else proportion of data randomly sampled. "
     exit 1
 }
 
-
-while getopts n:d:t:e:g:p:s: option
+while getopts n:d:t:l:e:g:p:s: option
 do
     case "${option}"
     in
 	n) NET=${OPTARG};;
 	d) DATASET=${OPTARG};;
 	t) TRIAL=${OPTARG};;
-	e) N_EPOCHS_TRAIN=$OPTARG;;
-	g) EPOCHS_TEST=$OPTARG;;
-	p) PERM_LABELS=$OPTARG;;
-	s) DATA_SUBSET=$OPTARG;;
+	l) LR=${OPTARG};;
+	e) N_EPOCHS_TRAIN=${OPTARG};;
+	g) EPOCHS_TEST=${OPTARG};;
+	p) PERM_LABELS=${OPTARG};;
+	s) DATA_SUBSET=${OPTARG};;
     esac
 done
 
-THRESHOLDS="0.95 0.90 0.85 0.80 0.75 0.70 0.65 0.60 0.55 0.50"
+source config.sh #define your static variables (SAVE_PATH, THRESHOLDS, LR, etc) in this file
 
 echo ""
 echo "----------------------------------------"
 echo "Training network"
 echo "----------------------------------------"
 echo ""
-python ../train.py --net $NET --dataset $DATASET --trial $TRIAL --epochs $N_EPOCHS_TRAIN --permute_labels $PERM_LABELS --subset $DATA_SUBSET 
+python ../train.py --net $NET --dataset $DATASET --trial $TRIAL --epochs $N_EPOCHS_TRAIN --lr $LR --permute_labels $PERM_LABELS --subset $DATA_SUBSET 
 
 echo ""
 echo "----------------------------------------"
 echo "Building graph"
 echo "----------------------------------------"
 echo ""
-python ../build_graph_functional.py --net $NET --dataset $DATASET --trial $TRIAL --epochs $EPOCHS_TEST --thresholds $THRESHOLDS 
+python ../build_graph_functional.py --net $NET --dataset $DATASET --trial $TRIAL --epochs $EPOCHS_TEST --thresholds $THRESHOLDS
 
 echo ""
 echo "----------------------------------------"
@@ -55,7 +55,7 @@ for e in $EPOCHS_TEST
 do
     for t in $THRESHOLDS
     do
-	./topology.sh $NET $DATASET $TRIAL $e $t 
+	eval "../cpp/symmetric "$SAVE_PATH$NET"_"$DATASET"/badj_epc"$e"_t"$t"_trl"$TRIAL".csv 1 0"      
     done
 done
 
@@ -65,7 +65,4 @@ echo "Prepare topology results"
 echo "----------------------------------------"
 echo ""
 
-for e in $EPOCHS_TEST
-do
-    python prepare_results.py --net $NET --dataset $DATASET --trial $TRIAL --epochs $e  --thresholds $THRESHOLDS --permute_labels $PERM_LABELS --subset $DATA_SUBSET
-done
+python prepare_results.py --path $SAVE_PATH --net $NET --dataset $DATASET --trial $TRIAL --epochs $EPOCHS_TEST  --thresholds $THRESHOLDS --permute_labels $PERM_LABELS --subset $DATA_SUBSET
