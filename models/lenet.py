4916dcb06893e4039245e3631a9b6cbd2bf5e1ea
CiprianCorneanu <CiprianCorneanu>
2019-01-25 21:26:26 +0100

    Two version of Lenet. one takes 28x28 inputs, the other 32x32 inputs.

--- models/lenet.py
+++ models/lenet.py
@@ -4,18 +4,19 @@ import torch.nn.functional as F
 
 
 class LeNet(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes, input_size=28):
         super(LeNet, self).__init__()
+        self.feat_size = 500 if input_size==32 else 320 if input_size==28 else -1
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(500, 50)
+        self.fc1 = nn.Linear(self.feat_size, 50)
         self.fc2 = nn.Linear(50, num_classes)
 
     def forward(self, x):
         out = F.relu(F.max_pool2d(self.conv1(x), 2))
         out = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(out)), 2))
-        out = out.view(-1, 500)
+        out = out.view(-1, self.feat_size)
         out = F.relu(self.fc1(out))
         '''out = F.dropout(out, training=self.training)'''
         out = F.log_softmax(self.fc2(out), dim=1)
@@ -24,7 +25,7 @@ class LeNet(nn.Module):
     def forward_features(self, x):
         x1 = F.relu(F.max_pool2d(self.conv1(x), 2))
         x2 = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x1)), 2))
-        x2 = x2.view(-1, 500)
+        x2 = x2.view(-1, self.feat_size)
         x3 = F.relu(self.fc1(x2))
         x4 = F.log_softmax(self.fc2(x3), dim=1)
         return [x1, x2, x3, x4]
