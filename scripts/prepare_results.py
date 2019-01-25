bfedafedc50c82ef02bb322d2ba41de7ac6dcc9a
CiprianCorneanu <CiprianCorneanu>
2019-01-25 21:21:40 +0100

    Refactoring.

--- scripts/prepare_results.py
+++ scripts/prepare_results.py
@@ -1,43 +1,34 @@
 import os
 import argparse
 import pickle
+from bettis import *
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser = argparse.ArgumentParser()
 parser.add_argument('--net')
 parser.add_argument('--dataset')
 parser.add_argument('--trial', default=0, type=int)
 parser.add_argument('--epochs', nargs='+', type=int)
-parser.add_argument('--thresholds', nargs='+')
-parser.add_argument('--n_bettis', type=int, default=3)
-parser.add_argument('--permute_labels', type=float)
-parser.add_argument('--subset', type=float)
+parser.add_argument('--thresholds', nargs='+', type=float)
+parser.add_argument('--permute_labels', type=float, default=0)
+parser.add_argument('--subset', type=float, default=1)
 parser.add_argument('--path')
 args = parser.parse_args()
 
 path = args.path + 'adjacency/' + args.net + '_' + args.dataset + '/'
+thresholds = [float(x) for x in args.thresholds]
 
+""" Compile bettis from correspoding file and prepare for saving"""
 bettis = {}
 for e in args.epochs:
     epoch = {}
-    for t in args.thresholds:
-        
-        """ Read bettis from correspoding file """
-        fname = path + 'badj_epc{}_t{}_trl{}.csv_symmetric_bettis.txt'.format(e, t, args.trial)
-        
-        with open(fname) as f:
-            content = f.readlines()
+    for t in thresholds:
+        fname = path + 'badj_epc{}_t{:1.2f}_trl{}.csv_symmetric_bettis.txt'.format(e, t, args.trial)
+        epoch['t_{:1.2f}'.format(t)] =  compile_bettis(fname, n_bettis=3)        
 
-        content = content[0].split(',')[1:1+args.n_bettis]
-        
-        content_ints = []
-        for x in content:
-            try:
-                content_ints.append(int(x))
-            except ValueError:
-                content_ints.append(0)
-        
-        epoch['t_{}'.format(t)] = content_ints
-    bettis['epc_{}'.format(e)] = epoch
+    measure = betti_integral(epoch, min(thresholds), max(thresholds))
+    bettis['epc_{}'.format(e)] = (epoch, measure)
+    
+    print('topology measure={}'.format(measure))
 
 ''' Save bettis '''
 with open(path + 'bettis_trl{}_p{}_s{}.pkl'.format(args.trial, args.permute_labels, args.subset), "wb") as f:
