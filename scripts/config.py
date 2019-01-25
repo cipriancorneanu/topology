6433340b4e323d949932fd77543424a49e4365fc
CiprianCorneanu <CiprianCorneanu>
2019-01-25 21:19:30 +0100

    New python file for configuring local hyperparameters.

---
+++ scripts/config.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# For what threholds I am going to create binary graphs for computing topology
+THRESHOLDS = np.arange(0.50, 1, 0.05)
+
+# Where to save topology results
+SAVE_PATH='/data/data1/datasets/cvpr2019/'
