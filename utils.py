725e53b22b29f0708900fd6852d9eaa9b8c0f9d6
CiprianCorneanu <CiprianCorneanu>
2019-02-04 22:56:24 +0100

    os.popen throws exception if run wuth nohup. exception handleded.

--- utils.py
+++ utils.py
@@ -47,8 +47,11 @@ def init_params(net):
             if m.bias:
                 init.constant(m.bias, 0)
 
-
-_, term_width = os.popen('stty size', 'r').read().split()
+try:
+    _, term_width = os.popen('stty size', 'r').read().split()
+except ValueError:
+    term_width = 0
+    
 term_width = int(term_width)
 
 TOTAL_BAR_LENGTH = 65.
