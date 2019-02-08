bf828a4c4706bd2a8abc552e355c332c80207625
CiprianCorneanu <CiprianCorneanu>
2019-02-08 21:26:44 +0100

    Introducing cifar10_gray.

--- models/utils.py
+++ models/utils.py
@@ -6,14 +6,37 @@ from .densenet import *
 from .inception import *
 
 
+
+num_classes={'mnist':10,
+         'cifar10':10,
+         'cifar10_gray':10, 
+         'mnist_adversarial':10,
+         'imagenet':200,
+         'imagenet_gray':200,
+         'vgg_cifar10_adversarial':10}
+
+'''
+def get_model(name, dataset):
+    if name=='lenet':
+        net = LeNet(num_classes[dataset])
+    if name=='lenet32':
+        net = LeNet(num_classes[name])
+    if name=='lenetext':
+        net = LeNetExt()
+'''
+
 def get_model(name, dataset):
     if name=='lenet' and dataset in ['mnist', 'cifar10', 'mnist_adversarial']:
         net = LeNet(num_classes=10)
     if name=='lenet' and dataset == 'imagenet':
         net = LeNet(num_classes=200)
-    if name=='lenet32' and dataset in ['mnist', 'cifar10', 'mnist_adversarial']:
+    if name=='lenetbin':
+        net = LeNet(num_classes=2)
+    if name=='lenet32' and dataset in ['mnist', 'cifar10_gray', 'mnist_adversarial']:
         net = LeNet(num_classes=10, input_size=32)
-    if name=='lenet32' and dataset == 'imagenet':
+    if name=='lenet32bin':
+        net = LeNet(num_classes=2, input_size=32)
+    if name=='lenet32' and dataset == 'imagenet_gray':
         net = LeNet(num_classes=200, input_size=32)
     if name=='lenetext' and dataset=='mnist':
         net = LeNetExt(n_channels=1, num_classes=10)
@@ -45,9 +68,9 @@ def get_model(name, dataset):
 
 def get_criterion(dataset):
     ''' Prepare criterion '''
-    if dataset in ['cifar10', 'imagenet']:
+    if dataset in ['cifar10', 'cifar10_gray', 'imagenet']:
         criterion = nn.CrossEntropyLoss()
-    elif dataset in ['mnist', 'mnist_adverarial']:
+    elif dataset in ['mnist', 'mnist_adversarial']:
         criterion = F.nll_loss
         
     return criterion 
