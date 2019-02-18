5736e8d60109ae810350bcef1ca6e16c198d39ed
CiprianCorneanu <CiprianCorneanu>
2019-02-18 18:02:54 +0100

    Factorizing the graph building scripts.

--- build_graph_functional.py
+++ build_graph_functional.py
@@ -17,6 +17,7 @@ from passers import Passer
 from savers import save_activations, save_checkpoint, save_losses
 from loaders import *
 from graph import *
+from labels import load_manipulator
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
 parser.add_argument('--net')
@@ -24,11 +25,12 @@ parser.add_argument('--dataset')
 parser.add_argument('--save_path')
 parser.add_argument('--trial', default=0, type=int)
 parser.add_argument('--epochs', nargs='+', type=int)
-parser.add_argument('--split', type=int, default=0)
-parser.add_argument('--kl', type=int, default=0)
+parser.add_argument('--split', default=0, type=int)
+parser.add_argument('--kl', default=0, type=int)
 parser.add_argument('--input_size', default=32, type=int)
 parser.add_argument('--thresholds', nargs='+', type=float)
-parser.add_argument('--graph_type', default='functional')
+parser.add_argument('--permute_labels', default=0, type=float)
+parser.add_argument('--binarize_labels', default=-1, type=int)
 
 args = parser.parse_args()
 
@@ -54,12 +56,14 @@ if device == 'cuda':
     cudnn.benchmark = True
 
 ''' Prepare criterion '''
-if args.dataset in ['cifar10', 'vgg_cifar10_adversarial', 'imagenet']:
+if args.dataset in ['cifar10', 'cifar10_gray', 'vgg_cifar10_adversarial', 'imagenet']:
     criterion = nn.CrossEntropyLoss()
 elif args.dataset in ['mnist', 'mnist_adverarial']:
     criterion = F.nll_loss
 
-
+''' Define label manipulator '''
+manipulator = load_manipulator(args.permute_labels, args.binarize_labels)
+    
 for epoch in args.epochs:
     print('==> Loading checkpoint for epoch {}...'.format(epoch))
     assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
@@ -67,64 +71,15 @@ for epoch in args.epochs:
     net.load_state_dict(checkpoint['net'])
     
     ''' Define passer and get activations '''
-    functloader = loader(args.dataset+'_test', batch_size=100, subset=list(range(0, 1000)))
+    functloader = loader(args.dataset+'_test', subset=list(range(0, 1000)))
     passer = Passer(net, functloader, criterion, device)
+    passer_test = Passer(net, functloader, criterion, device)
  
-    if args.graph_type == 'functional':
-        activs = passer.get_function()
-
-        for i, a in enumerate(activs):
-            print('{}: {}'.format(i, a.shape))
-
-        ''' If high number of nodes compute adjacency on layers and chunks'''
-        ''' Treat all network at once or split it into chunks and treat each '''
-        if not args.split:
-            activs = signal_concat(activs)
-            adj = adjacency(activs)
+    passer_test.run(manipulator=manipulator)
+    activs = passer.get_function()
+    activs = signal_concat(activs)
+    adj = adjacency(activs)
             
-            for threshold in THRESHOLDS:
-                badj = binarize(np.copy(adj), threshold)
-                print('t={} s={}'.format(threshold, np.sum(badj)))
-                np.savetxt(SAVE_DIR + 'badj_epc{}_t{:1.2f}_trl{}.csv'.format(epoch, threshold, args.trial), badj, fmt='%d', delimiter=",")
-        else:
-            splits = signal_splitting(activs, args.split)
-            
-            if not args.kl:
-                ''' Compute correlation metric for each split'''
-                adjs = [[adjacency(x) for x in layer] for layer in splits]
-                for threshold in THRESHOLDS:
-                    save_splits(adjs, args.split, SAVE_DIR, START_LAYER, epoch, threshold, args.trial)
-            else:
-                ''' Compute KL divergence between correlation distribution of each pair of splits '''
-                adj = adjacency_kl(splits)
-                for threshold in THREHSOLDS:
-                    np.savetxt(SAVE_DIR + 'badj_epc{}_t{:1.2f}_trl{}.csv'.format(epoch, threshold, args.trial), adj, fmt='%d', delimiter=",")
-    elif args.graph_type == 'structural':
-        weights = passer.get_structure()
-        ''' If high number of nodes compute adjacency on layers and chunks'''
-        ''' Treat all network at once or split it into chunks and treat each '''
-        if not args.split:
-            splits = signal_dimension_adjusting(weights,weights[0].shape[1])
-            print("Splits number:{}".format(splits[0].shape)) 
-            weights = signal_concat(splits)
-            
-            adj = adjacency(weights)
-            
-            for threshold in THRESHOLDS:
-                badj = binarize(np.copy(adj), threshold)
-                print('t={} s={}'.format(threshold, np.sum(badj)))
-                np.savetxt(SAVE_DIR + 'badj_epc{}_t{:1.2f}_trl{}.csv'.format(epoch, threshold, args.trial), badj, fmt='%d', delimiter=",")
-        else:
-            print("weights shape as : {}, {}".format(weights[0].shape,weights[0].shape[1]))
-            splits = signal_splitting(weights, weights[0].shape[1])
-            
-            if not args.kl:
-                ''' Compute correlation metric for each split'''
-                adjs = [[adjacency(x) for x in layer] for layer in splits]
-                for threshold in THRESHOLDS:
-                    save_splits(adjs, args.split, SAVE_DIR, START_LAYER, epoch, threshold, args.trial)
-            else:
-                ''' Compute KL divergence between correlation distribution of each pair of splits '''
-                adj = adjacency_kl(splits)
-                for threshold in THREHSOLDS:
-                    np.savetxt(SAVE_DIR + 'badj_epc{}_t{:1.2f}_trl{}.csv'.format(epoch, threshold, args.trial), adj, fmt='%d', delimiter=",")        
+    for threshold in args.thresholds:
+        badj = binarize(np.copy(adj), threshold)
+        np.savetxt(SAVE_DIR + 'badj_epc{}_t{:1.2f}_trl{}.csv'.format(epoch, threshold, args.trial), badj, fmt='%d', delimiter=",")
