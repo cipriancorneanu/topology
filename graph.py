7786094ea53855bb2a49ff87364c5d39db053089
CiprianCorneanu <CiprianCorneanu>
2019-03-07 17:21:19 +0100

    Adding functions for recovering structure graph from neural networks. First version, functional but probably buggy. Seems slow for conv2d.

--- graph.py
+++ graph.py
@@ -68,7 +68,7 @@ def binarize(M, binarize_t):
     ''' Binarize matrix. Real subunitary values. '''
     M[M>binarize_t] = 1
     M[M<=binarize_t] = 0
-
+    
     return M
     
 
@@ -152,14 +152,14 @@ def signal_concat(signals):
 def adjacency_correlation_distribution(splits, metric):            
     ''' Get correlation distribution for each split and build adjacency matrix between
     set of chunks using metric between distributions. '''
-    
+
     ''' Compute correlation pdfs per split '''
     corrpdfs = [corrpdf(x) for layer in splits for x in layer]
     
     ''' Compute adjacency matrix'''
     A = adjacency(np.asarray(corrpdfs), metric=metric)
     
-    return A
+    return robust_scaler(A)
 
 
 def build_density_adjacency(activs, density_t):
@@ -186,3 +186,188 @@ def build_density_adjacency(activs, density_t):
         t = t-t_decr
         
     return corr, density, t
+
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+
+def get_discriminative_nodes(X, Y, ratio):
+    X = X.astype(np.float64)
+    clf = LinearDiscriminantAnalysis(solver='lsqr', shrinkage=0.001)
+    
+    print(np.max(X), np.min(X), np.mean(X))
+    print(np.sum(X))
+
+    clf.fit(X, Y)
+    print('LDA score is {}'.format(clf.score(X, Y)))    
+
+    return np.argsort(np.abs(clf.coef_))[:, -int(ratio*clf.coef_.size):]        
+
+
+def get_index(shape, position):
+    ''' '''
+    return int(position[0]*shape[2]*shape[3] + position[1]*shape[3] + position[2])
+
+
+def structure_conv2d(in_layer, out_layer, weights, kernel_size, stride, padding):
+    ''' 
+    Build adjacency matrix for a conv2d operation between two tensors.
+    Comment: in this form padding and dilation are ignored and 
+    kernel_size and stride are cosidered square that is stride[0]=stride[1]
+    '''
+
+    a = np.zeros((np.prod(in_layer.shape), np.prod(out_layer.shape)))
+    '''
+    print("structure built has shape {}".format(a.shape))
+    print("weights have shape {}".format(weights.shape))
+    '''
+    n_in_channels, in_width, in_height = in_layer.shape[1:]
+    n_out_channels, out_width, out_height = out_layer.shape[1:]
+    
+    ''' Input sliding window center coordinates '''
+    i_star_list = [int(i) for i in np.arange((kernel_size[0]-1)/2, in_width-(kernel_size[0]-1)/2, stride[0])]
+    j_star_list = [int(j) for j in np.arange((kernel_size[0]-1)/2, in_width-(kernel_size[0]-1)/2, stride[0])]
+
+    ''' Output sliding window targets '''
+    r_star_list = int((kernel_size[0]-1)/2) + np.asarray([i for i,_ in enumerate(i_star_list)], dtype=int)
+    s_star_list = int((kernel_size[0]-1)/2) + np.asarray([i for i,_ in enumerate(j_star_list)], dtype=int)
+
+    ''' Input space '''
+    in_coords = [[x for x in itertools.product(np.arange(n_in_channels),np.arange(i_star-(kernel_size[0]-1)/2, i_star+(kernel_size[1]+1)/2),
+                                               np.arange(j_star-(kernel_size[1]-1)/2, j_star+(kernel_size[1]+1)/2))] for i_star,j_star in itertools.product(i_star_list, j_star_list)]
+
+    ''' Output space '''
+    out_coords = [[x for x in itertools.product([r_star], [s_star], np.arange(n_out_channels))] for r_star, s_star in itertools.product(r_star_list, s_star_list)]
+
+    ''' Iterate though input output pairs and fill structure '''
+    weights = weights.data.cpu().numpy()
+    for i, (inc,otc) in enumerate(zip(in_coords, out_coords)):
+        '''
+        print('Filling pair {}/{}'.format(i,len(in_coords)))
+        print('{}:{} > {}'.format(i,inc,otc))
+        print('{}> {}'.format(len(inc), len(otc)))
+        print('-'*32)
+        '''
+        for i in range(n_out_channels):
+            linear_x = [get_index(in_layer.shape, x) for x in inc]
+            linear_y = get_index(out_layer.shape, otc[i])  
+            '''print('linear_x: {} / linear_y: {}'.format(linear_x, linear_y))'''
+            for j in range(n_in_channels):
+                for ix,x in enumerate(linear_x[j*np.prod(kernel_size):(j+1)*np.prod(kernel_size)]):
+                    q,r = divmod(ix,kernel_size[0])
+                    '''print('a[{},{}] = weights[{},{},{},{}]'.format(x,linear_y,i,j,q,r))'''
+                    a[x,linear_y] = weights[i,j,q,r]
+    return a
+
+
+def structure_maxpool2d(in_layer, out_layer, kernel_size, stride, padding, dilation):
+    ''' 
+    Build adjacency matrix for a maxpool2d operation between two tensors.
+    Very similar to conv2d. They could be a slingle more abstract function.
+    Comment: in this form padding and dilation are ignored and 
+    kernel_size and stride are cosidered square that is stride[0]=stride[1]
+    '''
+
+    a = np.zeros((np.prod(in_layer.shape), np.prod(out_layer.shape)))
+    n_in_channels, in_width, in_height = in_layer.shape[1:]
+    n_out_channels, out_width, out_height = out_layer.shape[1:]
+    
+    ''' Input sliding window center coordinates '''
+    i_star_list = j_star_list = [int(i) for i in np.arange((kernel_size-1)/2, in_width-(kernel_size-1)/2, stride)]
+
+    ''' Output sliding window targets '''
+    r_star_list = s_star_list = int(kernel_size/2) + np.asarray([i for i,_ in enumerate(i_star_list)], dtype=int)
+
+    '''print('i_star_list {}\n j_star_list {}\n r_star_list {}\n s_star_list {}\n'.format(i_star_list, j_star_list, r_star_list, s_star_list))'''
+    
+    ''' Input space '''
+    in_coords = [[x for x in itertools.product(np.arange(n_in_channels),np.arange(i_star-(kernel_size-1)/2, i_star+(kernel_size+1)/2),
+                                               np.arange(j_star-(kernel_size-1)/2, j_star+(kernel_size+1)/2))] for i_star,j_star in itertools.product(i_star_list, j_star_list)]
+
+    ''' Output space '''
+    out_coords = [[x for x in itertools.product([r_star], [s_star], np.arange(n_out_channels))] for r_star, s_star in itertools.product(r_star_list, s_star_list)]
+
+    ''' Iterate though input output pairs and fill structure '''
+    for i, (inc,otc) in enumerate(zip(in_coords, out_coords)):
+        '''
+        print(i)
+        print('{}:{} > {}'.format(i,inc,otc))
+        print('{}> {}'.format(len(inc), len(otc)))
+        print('-'*32)
+        '''
+        for i in range(n_out_channels):
+            linear_x = [get_index(in_layer.shape, x) for x in inc]
+            linear_y = get_index(out_layer.shape, otc[i])  
+            '''print('linear_x: {} / linear_y: {}'.format(linear_x, linear_y))'''
+            for j in range(n_in_channels):
+                for ix,x in enumerate(linear_x[j*np.prod(kernel_size):(j+1)*np.prod(kernel_size)]):
+                    q,r = divmod(ix,kernel_size)
+                    '''print('a[{},{}] = weights[{},{},{},{}]'.format(x,linear_y,i,j,q,r))'''
+                    a[x,linear_y] = 1
+
+    return a
+
+
+def structure_linear(weights):
+    ''' Adjacency matrix for a linear operation (fully connected layer)'''
+    return np.transpose(weights)
+
+
+def get_node_number(model, x):
+    ''' Get total number of nodes of model for a specific input '''
+    return np.sum([np.prod(np.asarray(x.shape)) for x in model.forward_all_features(x)])
+
+
+def get_model_view(model, x, view):
+    ''' 
+    Given a model, an input and a list of layers returns 
+    the corresponding set of operations and tensors. Operations 
+    that do not have structure  (e.g Relu, BatchNorm) are ignored 
+    '''
+    layers = [layer for layer in [model.get_layers()[i] for i in view]
+              if type(layer).__name__ in ['Conv2d', 'MaxPool2d', 'Linear']]
+    print('All layers: {}. View layers indices: {}. Layers with structure: {}'.format(len(model.get_layers()), view, len(layers)))
+
+    tensors = [tensor for tensor in [model.forward_all_features(x)[i] for i in [view[0]-1]+view+[view[-1]+1]
+                                     if type(model.get_layers()[i]).__name__ in ['Conv2d', 'MaxPool2d', 'Linear']]]    
+    print('There are {} tensors in the model'.format(len(tensors)))
+
+    return layers, tensors
+
+
+def structure_from_view(model, x, view):
+    '''
+    Given a model (neural network), an input x <tensor> and a view <list of integers>
+    return adjacency matrix that defines a graph correspoding to the structure 
+    of the model.
+    '''
+    layers, tensors = get_model_view(model, x, view)
+    n_nodes = [np.prod(x.shape) for x in tensors]
+    print('There are {} nodes in the considered view of the model'.format(np.sum(n_nodes)))
+    A = np.zeros((np.sum(n_nodes), np.sum(n_nodes)))
+        
+    for i, (x, y, layer) in enumerate(zip(tensors[:-1], tensors[1:], layers)):
+        layer_type = type(layer).__name__
+        print('Layer {}: {} -> {} -> {}'.format(i, x.shape, layer_type, y.shape))
+        
+        if layer_type == 'Conv2d':
+            weights, kernel_size, stride, padding = list(layer.parameters())[0], layer.kernel_size, layer.stride, layer.padding
+            a = structure_conv2d(x, y, weights, kernel_size, stride, padding)
+        elif layer_type == 'MaxPool2d':
+            kernel_size, stride, padding, dilation = layer.kernel_size, layer.stride, layer.padding, layer.dilation 
+            a = structure_maxpool2d(x, y, kernel_size, stride, padding, dilation)
+        elif layer_type == 'Linear':
+            weights = list(layer.parameters())[0]
+            a = structure_linear(weights)
+        else:
+            pass
+        
+        ''' Fill structure '''        
+        acc_n_nodes = [int(np.sum(n_nodes[:i])) for i in range(len(n_nodes)+1)]
+        print(acc_n_nodes)
+        start_in, end_in, start_out, end_out = acc_n_nodes[i], acc_n_nodes[i+1], acc_n_nodes[i+1], acc_n_nodes[i+2]
+        print(start_in, end_in, start_out, end_out)
+        print('A.shape={}, a.shape={}'.format(A.shape, a.shape))
+        A[start_in:end_in, start_out:end_out] = a
+        
+    return A
+
