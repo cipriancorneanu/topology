b622e8285a65b508d63e5dd1fc7ea80320fa2fb2
CiprianCorneanu <CiprianCorneanu>
2019-04-10 16:20:32 +0200

    Adding Lenet_300_100.

--- models/lenet.py
+++ models/lenet.py
@@ -3,6 +3,50 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class LeNet_300_100(nn.Module):
+    def __init__(self, num_classes, input_size=28):
+        super(LeNet_300_100, self).__init__()
+        self.feat_size = 1024 if input_size==32 else 784 if input_size==28 else -1
+        self.fc1 = nn.Linear(self.feat_size, 300)
+        self.fc2 = nn.Linear(300, 100)
+        self.fc3 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        out = F.relu(self.fc1(x.view(-1,self.feat_size)))
+        out = F.relu(self.fc2(out))
+        out = F.log_softmax(self.fc3(out), dim=1)
+        return out
+    
+    def forward_features(self, x):
+        x1 = F.relu(self.fc1(x.view(-1, self.feat_size)))
+        x2 = F.relu(self.fc2(x1))
+        x3 = F.log_softmax(self.fc3(x2), dim=1)
+
+        return [x1, x2, x3]
+
+''' TO DO!!!!'''
+class LeNet5(nn.Module):
+    def __init__(self, num_classes, input_size=28):
+        super(LeNet5, self).__init__()
+        self.feat_size = 500 if input_size==32 else 320 if input_size==28 else -1
+        self.fc1 = nn.Linear(self.feat_size, 300)
+        self.fc2 = nn.Linear(300, 100)
+        self.fc3 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        out = F.relu(self.fc1(x))
+        out = F.relu(self.fc2(out))
+        out = F.log_softmax(self.fc3(out))
+        return out
+    
+    def forward_features(self, x):
+        x1 = F.relu(self.fc1(x))
+        x2 = F.relu(self.fc2(out))
+        x3 = F.log_softmax(self.fc3(out))
+
+        return [x1, x2, x3]
+    
+    
 class LeNet(nn.Module):
     def __init__(self, num_classes, input_size=28):
         super(LeNet, self).__init__()
@@ -64,13 +108,13 @@ class LeNetExt(nn.Module):
 
     
 def test():
-    net = LeNet(num_classes=10)
+    net = LeNet_300_100(num_classes=10)
     print(net)
     x = torch.randn(1,1,28,28)
     y = net(x)
     print(y.size())
 
-    for i, layer in enumerate(net.get_state(x)):
+    for i, layer in enumerate(net.forward_features(x)):
         print('layer {} has size {}'.format(i, layer.shape))
 
 '''test()'''
